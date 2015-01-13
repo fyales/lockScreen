bf5d02e6214f4e4d0c6535b17f35596832ac4478
fyales <weiweishen@anjuke.com>
2015-01-13 10:37:07 +0800

    更改图标，添加签名

--- app/src/main/java/fyales/com/lockscreen/MainActivity.java
+++ app/src/main/java/fyales/com/lockscreen/MainActivity.java
@@ -1,81 +1,57 @@
-package fyales.com.lockscreen;
+    package fyales.com.lockscreen;
 
-import android.app.Activity;
-import android.app.admin.DevicePolicyManager;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.os.*;
-import android.os.Process;
-import android.view.Menu;
-import android.view.MenuItem;
+    import android.app.Activity;
+    import android.app.admin.DevicePolicyManager;
+    import android.content.ComponentName;
+    import android.content.Context;
+    import android.content.Intent;
+    import android.os.*;
+    import android.os.Process;
 
 
-public class MainActivity extends Activity {
-    private DevicePolicyManager devicePolicyManager;
-    private ComponentName componentName;
-    private final static int LOCK_SCREEN_CODE = 201;
+    public class MainActivity extends Activity {
+        private DevicePolicyManager devicePolicyManager;
+        private ComponentName componentName;
+        private final static int LOCK_SCREEN_CODE = 201;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        devicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-        componentName = new ComponentName(this,DeviceFyalesReceiver.class);
-        lockScreen();
-        android.os.Process.killProcess(Process.myPid());
+            devicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+            componentName = new ComponentName(this,DeviceFyalesReceiver.class);
+            lockScreen();
+            android.os.Process.killProcess(Process.myPid());
 
-    }
-
-    /**
-     * 实现系统锁屏
-     */
-    private void lockScreen(){
-        if (devicePolicyManager.isAdminActive(componentName)){
-            devicePolicyManager.lockNow();
-        }else{
-             Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-             intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
-             startActivityForResult(intent, LOCK_SCREEN_CODE);
         }
 
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            switch (requestCode) {
-                case LOCK_SCREEN_CODE:
-                    devicePolicyManager.lockNow();
-                    break;
-                default:
-                    break;
+        /**
+         * 实现系统锁屏
+         */
+        private void lockScreen(){
+            if (devicePolicyManager.isAdminActive(componentName)){
+                devicePolicyManager.lockNow();
+            }else{
+                 Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                 intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+                 startActivityForResult(intent, LOCK_SCREEN_CODE);
             }
 
         }
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == Activity.RESULT_OK) {
+                switch (requestCode) {
+                    case LOCK_SCREEN_CODE:
+                        devicePolicyManager.lockNow();
+                        break;
+                    default:
+                        break;
+                }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
-}
